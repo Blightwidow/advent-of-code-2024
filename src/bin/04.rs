188ce46dbd649a1b04cdda044b6918e7174b283c
Theo Dammaretz <theo@dammaretz.fr>
2024-12-05 14:51:45 +0100

    refactor: use some bitboard for 4

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -3,18 +3,40 @@ use advent_of_code::utils::point::*;
 
 advent_of_code::solution!(4);
 
-// You could probably optimize this with bitboard as it looks a lot like
-// line scan for chess pieces.
+// You could probably optimize this with bitboard for diagonal as well
 pub fn part_one(input: &str) -> Option<u32> {
     let grid = Grid::parse(input);
     let mut result = 0;
+
+    fn scan_line(grid: &Grid<u8>, mut point: Point, step: Point, size: i32) -> u32 {
+        let mut bytes = 0;
+        let mut result = 0;
+
+        for _ in 0..size {
+            // Shift the bytes to the left and add the new byte.
+            // Elminiate the first to limit the size at 4 bytes thanks to the type u32.
+            bytes = (bytes << 8) | (grid[point] as u32);
+            point += step;
+            // "XMAS" and "SAMX" in hex.
+            result += (bytes == 0x584d4153 || bytes == 0x53414d58) as u32;
+        }
+
+        result
+    }
+
+    for i in 0..grid.width {
+        result += scan_line(&grid, Point::new(i, 0), DOWN, grid.height);
+    }
+    for i in 0..grid.height {
+        result += scan_line(&grid, Point::new(0, i), RIGHT, grid.width);
+    }
 
     for x in 0..grid.width {
         for y in 0..grid.height {
             let point = Point::new(x, y);
 
             if grid[point] == b'X' {
-                for step in DIAGONAL {
+                for &step in [DOWN + RIGHT, DOWN + LEFT, UP + RIGHT, UP + LEFT].iter() {
                     result += (grid.contains(point + step * 3)
                         && grid[point + step] == b'M'
                         && grid[point + step * 2] == b'A'
